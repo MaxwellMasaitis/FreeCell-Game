9d9cd27cba0570e2118e3bb149bd78afe2013bfd
MaxwellMasaitis <masaitism20@mail.wlu.edu>
2018-12-14 20:46:16 -0500

    Added Code

---
+++ ViewInformer.java
@@ -0,0 +1,16 @@
+/**
+ * project8
+ * Represents a view informer object.
+ * @author Maxwell
+ * @author Pan
+ * @author Annjoy
+ * @author Zander
+ * @version 1.0
+ */
+public interface ViewInformer {
+	/**
+     * Informs the main app when a panel is pressed.
+     * @param panel
+     */
+	public void panelPressed(CellPanel panel);
+}
